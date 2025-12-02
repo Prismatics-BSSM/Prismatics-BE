b549705ff3a19276ea8d2fcfb340bd0c5a7a2fdb
ketarubot <robin09v@naver.com>
2025-12-02 12:19:38 +0900

    feat: ElementService Controller 연결

--- src/main/java/org/science/prismatics/controller/ElementController.java
+++ src/main/java/org/science/prismatics/controller/ElementController.java
@@ -1,13 +1,29 @@
 package org.science.prismatics.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.science.prismatics.dto.response.ElementInfoResponse;
+import org.science.prismatics.dto.response.SpectrumInfoResponse;
 import org.science.prismatics.service.ElementService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/elements")
 public class ElementController {
     private final ElementService elementService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ElementInfoResponse> getElementInfoById(@PathVariable Integer id) {
+        ElementInfoResponse elementInfo = elementService.getElementInfoById(id);
+        return ResponseEntity.ok(elementInfo);
+    }
+
+    @GetMapping("/spectrums")
+    public ResponseEntity<SpectrumInfoResponse> getSpectrumInfoByIds(@RequestParam List<Integer> ids) {
+        SpectrumInfoResponse spectrum = elementService.getSpectrumByElementIds(ids);
+        return ResponseEntity.ok(spectrum);
+    }
 }
