2fe1274cba0845cbf73b7f70315fbc0efbbfc4cb
ketarubot <robin09v@naver.com>
2025-12-02 19:42:06 +0900

    chore: 디버그 코드 추가

--- src/main/java/org/science/prismatics/PrismaticsApplication.java
+++ src/main/java/org/science/prismatics/PrismaticsApplication.java
@@ -1,13 +1,28 @@
 package org.science.prismatics;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class PrismaticsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(PrismaticsApplication.class, args);
     }
 
+}
+
+@Component
+class DebugDbUrl implements CommandLineRunner {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("🔍 [DEBUG] spring.datasource.url = " + datasourceUrl);
+    }
 }
